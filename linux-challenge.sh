c9f3efc78640c5adfa19242e669e05a6cc624484
Joshua Carl Soguilon <163475044+rul3zero@users.noreply.github.com>
2025-10-17 00:28:42 +0800

    Clarify Instructions

--- linux-challenge.sh
+++ linux-challenge.sh
@@ -76,7 +76,8 @@ echo "USE COMMANDS: cd , ls, cat, grep, chmod"
 echo << "EOF"
 The linux-challenge directory has 5 folders and each of the folders have 5 files each.
 
-1. If you cant get inside the directory. What is the command you use to change the permissions?
+1. If you cant get inside the directory. What is the command you use to change the permissions? 
+--DONT USE SUDO PLEASE--
 2. You can manually "cat" every file. OR. You can use "grep" command.
 3. Look for a file that have the word "key".
 4. You can use recursive search: use "man <command>" and look for recursive.
